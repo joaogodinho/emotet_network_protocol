cfc2a95dc9a08e81749e023751a8a273947dfc2e
joaogodinho <joaogodinho@users.noreply.github.com>
2021-01-13 19:53:34 +0000

    Feat: Use argparse; Add pubkey and ips file as args

--- emutet/emutet.py
+++ emutet/emutet.py
@@ -7,7 +7,7 @@ import tempfile
 import datetime
 import logging
 from hashlib import sha1, md5
-from optparse import OptionParser
+from argparse import ArgumentParser
 
 from src.bot.emotet import EmotetEmulator
 import src.utils.emoutils as emoutils
@@ -30,7 +30,6 @@ def main(options):
     if options.cape:
         print("[+] https://www.capesandbox.com/configdownload/{0}/Emotet/".format(options.cape))
         tmpf = emoutils.get_analysis_from_cape(options.cape)
-
     elif options.triage:
         api_key_filename = options.triage_api_key if options.triage_api_key else os.path.join(os.path.dirname(__file__), "src", "config/triage-api-key.txt")
 
@@ -41,14 +40,17 @@ def main(options):
         print("[+] https://tria.ge/reports/{0}/task1".format(options.triage))
         tmpf = emoutils.get_analysis_from_triage(options.triage, api_key_filename)
 
-    if tmpf == "":
+    if (options.cape or options.triage) and tmpf == "":
         print("[!] Error! Can't get the configuation")
         return
 
     if options.cape:
-        emutet = EmotetEmulator(1, 2, cape=tmpf)
-    if options.triage:
-        emutet = EmotetEmulator(1, 2, triage=tmpf)
+        emutet = EmotetEmulator(cape=tmpf)
+    elif options.triage:
+        emutet = EmotetEmulator(triage=tmpf)
+    else:
+        if options.rsa and options.ips:
+            emutet = EmotetEmulator(keyfilename=options.rsa, ipsfilename=options.ips)
 
     modules = emutet.download(options.try_all)
     print("[+] Downloaded {n} modules".format(n=len(modules)))
@@ -62,23 +64,25 @@ def main(options):
 
 
 if __name__ == '__main__':
-    parser = OptionParser()
-    parser.add_option("-c", "--cape", type="int", default=None, help="Analysis ID from https://www.capesandbox.com")
-    parser.add_option("-t", "--triage", type="str", default=None, help="Analysis ID from https://www.tria.ge")
-    parser.add_option("-k", "--triage-api-key", type="str", default=None, help="Triage API key file. To get yours go to https://tria.ge and ask for an account")
-    parser.add_option("-o", "--output", type="str", default=None, help="Output directory")
-    parser.add_option("-v", "--verbose", action="store_true", default=False, help="Verbose mode. Shows errors and debugging prints")
-    parser.add_option("-T", "--try-all", action="store_true", default=False, help="Try all C&C in the C&C list. By default the bot will stop once" \
+    parser = ArgumentParser()
+    parser.add_argument("-r", "--rsa", type=str, default=None, help="RSA key file (use with -i)")
+    parser.add_argument("-i", "--ips", type=str, default=None, help="C2s list file (host:port per line)")
+    parser.add_argument("-c", "--cape", type=int, default=None, help="Analysis ID from https://www.capesandbox.com")
+    parser.add_argument("-t", "--triage", type=str, default=None, help="Analysis ID from https://www.tria.ge")
+    parser.add_argument("-k", "--triage-api-key", type=str, default=None, help="Triage API key file. To get yours go to https://tria.ge and ask for an account")
+    parser.add_argument("-o", "--output", type=str, default=None, help="Output directory")
+    parser.add_argument("-v", "--verbose", action="store_true", default=False, help="Verbose mode. Shows errors and debugging prints")
+    parser.add_argument("-T", "--try-all", action="store_true", default=False, help="Try all C&C in the C&C list. By default the bot will stop once" \
                                                                                   "a sucess response is gotten from the C&C. This can be when it" \
                                                                                   " downloads new modules or when the C&C response is empty." \
                                                                                   " If this option is enabled the bot doesn't stop until all C&C" \
                                                                                   " responses are checked")
 
-    (options, args) = parser.parse_args()
+    options = parser.parse_args()
 
-    if options.cape == None and options.triage == None:
-        print("[!] Analysis ID not provided")
-        print(parser.usage)
+    if options.cape == None and options.triage == None and (options.rsa == None or options.ips == None):
+        print("[!] Analysis ID and RSA and IP file not provided")
+        parser.print_help()
         exit()
 
     if options.verbose:
